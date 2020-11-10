9038802cfd9015cb2588d7e35c0a5d0dc05c2ca1
S. Amaador <soufiane.amaador@hva.nl>
2020-11-11 00:53:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/madlevel5example/ReminderViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.madlevel5example
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ReminderViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val ioScope = CoroutineScope(Dispatchers.IO)
+    private val reminderRepository = ReminderRepository(application.applicationContext)
+
+    val reminders: LiveData<List<Reminder>> = reminderRepository.getAllReminders()
+
+    fun insertReminder(reminder: Reminder) {
+        ioScope.launch {
+            reminderRepository.insertReminder(reminder)
+        }
+    }
+
+    fun deleteReminder(reminder: Reminder) {
+        ioScope.launch {
+            reminderRepository.deleteReminder(reminder)
+        }
+    }
+ }
---
+++ app/src/main/java/com/example/madlevel5example/RemindersFragment.kt
@@ -0,0 +1,103 @@
+package com.example.madlevel5example
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel4example.R
+import kotlinx.android.synthetic.main.fragment_reminders.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class RemindersFragment : Fragment() {
+
+
+    private val reminders = arrayListOf<Reminder>()
+    private val reminderAdapter = ReminderAdapter(reminders)
+
+    private val viewModel: ReminderViewModel by viewModels()
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reminders, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+        observeAddReminderResult()
+
+
+    }
+
+
+    private fun initViews(){
+        rvReminders.layoutManager =
+            LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rvReminders.adapter = reminderAdapter
+        rvReminders.addItemDecoration(DividerItemDecoration(context,DividerItemDecoration.VERTICAL))
+
+        createItemTouchHelper().attachToRecyclerView(rvReminders)
+    }
+
+    private fun observeAddReminderResult() {
+        viewModel.reminders.observe(viewLifecycleOwner, Observer{
+            reminders ->
+            this@RemindersFragment.reminders.clear()
+            this@RemindersFragment.reminders.addAll(reminders)
+            reminderAdapter.notifyDataSetChanged()
+        })
+    }
+
+    /**
+     * Create a touch helper to recognize when a user swipes an item from a recycler view.
+     * An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+     * and uses callbacks to signal when a user is performing these actions.
+     */
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+//                reminders.removeAt(position)
+//                reminderAdapter.notifyDataSetChanged()
+                val reminderToDelete = reminders[position]
+                viewModel.deleteReminder(reminderToDelete)
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+
+}
